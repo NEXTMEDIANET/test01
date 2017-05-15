e967bb2984257d499deb5f77573bb5c0d36ea81f
Cesar <cesar.valladares@soulnetsolutions.com>
2017-05-15 09:42:38 +0200

    cambio 1

--- archivo1.hs
+++ archivo1.hs
@@ -4,11 +4,16 @@ enteros = iterate (+1) 0
 enteros :: Int
 enteros = iterate (+1) 0
 
+
+
 enteros :: Int
 enteros = iterate (+1) 0
 
 enteros :: Int
 enteros = iterate (+1) 0
+
+
+//esto es una prueba, 
 
 enteros :: Int
 enteros = iterate (+1) 0
